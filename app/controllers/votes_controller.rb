7754a8d0991c67f5db29b09dd8c3118bfe9a19dc
Ryan Bigg <radarlistener@gmail.com>
2008-06-06 19:19:44 +0930

    More twonking goodness. People can now vote against the fact that a person is a twonk. Needs protection, but otherwise fine.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,30 @@
 class VotesController < ApplicationController
+  before_filter :find_twonk
+  def new
+   @vote = @twonk.votes.build(:user_id => session[:user])
+   @vote.positive = params[:positive] 
+  end 
+
+  def create
+    @vote = @twonk.votes.build(params[:vote])
+    if @vote.save
+      if @vote.positive?
+      flash[:success] = "You think #{@twonk.name} is really a twonk."
+      @twonk.increment!(:vote_count)
+      else
+      flash[:success] = "You don't think #{@twonk.name} is really a twonk."
+      @twonk.decrement!(:vote_count)
+      end
+      redirect_to twonk_path(@twonk)
+    else
+      flash[:failure] = "Your vote failed. Try voting again!"
+      render :action => "new"
+    end
+  end
+
+  private
+
+    def find_twonk
+    @twonk = Twonk.find(params[:twonk_id]) unless params[:twonk_id].nil?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "twonks"
-  map.resources :twonks
+  map.resources :twonks do |twonk|
+    twonk.resources :votes
+  end
   map.resources :users
   map.resource :session
   map.connect ':controller/:action/:id'
