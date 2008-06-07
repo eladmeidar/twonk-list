375d7ea8394eee4b11c9cb465d861ea40f1d1580
ryan <ryan@ryan-desktop.(none)>
2008-06-07 17:15:48 +0930

    Added an about page, just a short blurb about what the site is.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.login 'login', :controller => "sessions", :action => "new"
   map.logout 'logout', :controller => "sessions", :action => "destroy"
   map.signup 'signup', :controller => "users", :action => "new"
+  map.about 'about', :controller => "pages", :action => "about"
   map.resources :twonks do |twonk|
     twonk.resources :votes
   end
