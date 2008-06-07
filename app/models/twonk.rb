22584940210d38a7374e07304bd45e1716d41b63
ryan <ryan@ryan-desktop.(none)>
2008-06-07 17:01:37 +0930

    Use TRUE instead of 't' and FALSE instead of 'f'

--- app/models/twonk.rb
+++ app/models/twonk.rb
@@ -1,7 +1,7 @@
 class Twonk < ActiveRecord::Base
   has_many :votes
-  has_many :for_votes, :class_name => "Vote", :conditions => "positive = 't'"
-  has_many :against_votes, :class_name => "Vote", :conditions => "positive = 'f'"
+  has_many :for_votes, :class_name => "Vote", :conditions => "positive = TRUE"
+  has_many :against_votes, :class_name => "Vote", :conditions => "positive = FALSE"
   has_many :voters, :through => :votes, :source => :user
   validates_presence_of :name, :location
   validates_uniqueness_of :name, :scope => :location
