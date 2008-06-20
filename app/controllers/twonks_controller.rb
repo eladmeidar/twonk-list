b04b9b8e6197e3eea6adab1195809664b35f8c95
Ryan Bigg <ryanbigg@ryan-macbook.local>
2008-06-20 09:40:57 +0930

    Added edit and delete links for twonks. Added a belongs_to :nominated_by to the twonk model so we can see who they were nominated by.

--- app/controllers/twonks_controller.rb
+++ app/controllers/twonks_controller.rb
@@ -1,5 +1,6 @@
 class TwonksController < ApplicationController
   before_filter :login_required, :except => [:index, :show]
+  before_filter :check_for_ownership, :only => [:edit, :update, :destroy]
   def index
     @top_twonks = Twonk.find(:all, :order => "votes_count DESC", :limit => 20)
     @most_recent_twonks = Twonk.find(:all, :order => "id DESC", :limit => 20)
@@ -24,7 +25,33 @@ class TwonksController < ApplicationController
     end
   end
   
+  def edit
+    #placeholder
+  end
+  
+  def update
+    if @twonk.update_attributes(params[:twonk])
+      flash[:success] = "Twonk has been updated."
+      redirect_to @twonk
+    else
+      flash[:failure] = "Twonk could not be updated."
+      render :action => "edit"
+    end
+  end
+  
   def show
     @twonk = Twonk.find(params[:id], :include => [:for_votes, :against_votes, :votes])
   end
+  
+  def destroy
+    @twonk.destroy
+  end
+  
+  def check_for_ownership
+    @twonk = Twonk.find(params[:id])
+    unless @twonk.nominated_by == current_user
+      flash[:notice] = "That is not your twonk to change!"
+      redirect_back_or_default(twonks_path)
+    end  
+  end 
 end
--- app/models/twonk.rb
+++ app/models/twonk.rb
@@ -3,6 +3,7 @@ class Twonk < ActiveRecord::Base
   has_many :for_votes, :class_name => "Vote", :conditions => "positive = TRUE"
   has_many :against_votes, :class_name => "Vote", :conditions => "positive = FALSE"
   has_many :voters, :through => :votes, :source => :user
+  belongs_to :nominated_by, :class_name => "User"
   attr_accessible :name, :location
   validates_presence_of :name, :location
   validates_uniqueness_of :name, :scope => :location
