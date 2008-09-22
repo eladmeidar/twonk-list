4ca80ecbef9644e5337801251ac2b610936d7f5f
Ryan Bigg <radarlistener@gmail.com>
2008-09-22 14:36:07 +0930

    Added very helpful destroy action.

--- app/controllers/admin/twonks_controller.rb
+++ app/controllers/admin/twonks_controller.rb
@@ -4,10 +4,15 @@ class Admin::TwonksController < Admin::ApplicationController
   end
   
   def accept
-    @twonk = Twonk.find(params[:id])
-    @twonk.accepted = true
-    @twonk.save
+    twonk = Twonk.find(params[:id])
+    twonk.accepted = true
+    twonk.save
     flash[:notice] = "Twonk #{@twonk.name} accepted."
     redirect_to admin_twonks_path
   end
+  
+  def destroy
+    Twonk.find(params[:id]).destroy
+    flash[:notice] = "Selected twonk has been deleted."
+    redirect_to admin_twonks_path
 end
