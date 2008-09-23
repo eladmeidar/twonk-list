8b0c8af31bdde71f0beeaca2356039b1844b51df
Ryan Bigg <radarlistener@gmail.com>
2008-09-23 09:53:36 +0930

    remove @

--- app/controllers/admin/twonks_controller.rb
+++ app/controllers/admin/twonks_controller.rb
@@ -7,7 +7,7 @@ class Admin::TwonksController < Admin::ApplicationController
     twonk = Twonk.find(params[:id])
     twonk.accepted = true
     twonk.save
-    flash[:notice] = "Twonk #{@twonk.name} accepted."
+    flash[:notice] = "Twonk #{twonk.name} accepted."
     redirect_to admin_twonks_path
   end
   
